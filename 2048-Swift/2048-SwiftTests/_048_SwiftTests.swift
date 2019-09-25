408f7a56ed5096130010667f4d47536e778e22bc
Alex Golub <alexeythumb@gmail.com>
2019-09-25 22:05:31 +0300

    Add Xcode project

---
+++ 2048-Swift/2048-SwiftTests/_048_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  _048_SwiftTests.swift
+//  2048-SwiftTests
+//
+//  Created by Alex Golub on 9/25/19.
+//  Copyright © 2019 Alex Golub. All rights reserved.
+//
+
+import XCTest
+@testable import _048_Swift
+
+class _048_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
