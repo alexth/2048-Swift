456e5b7bd7d6c809bae65c1424384b796722bbc2
Alex Golub <alexeythumb@gmail.com>
2019-10-04 22:40:12 +0300

    Add NodeView animations

--- 2048-Swift/2048-Swift/Views/NodeView.swift
+++ 2048-Swift/2048-Swift/Views/NodeView.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
+enum NodeViewAnimation {
+    case appearance
+    case disappearance
+    case move
+    case merge
+}
+
 final class NodeView: UIView {
     let index: Index
     let value: UInt
+    
+    private let animationDuration = TimeInterval(0.2)
     
     init(index: Index, frame: CGRect, value: UInt) {
         self.index = index
@@ -32,8 +41,35 @@ final class NodeView: UIView {
         let label = UILabel(frame: bounds)
         label.textAlignment = .center
         label.adjustsFontSizeToFitWidth = true
-        label.font = UIFont.systemFont(ofSize: 30)
+        label.font = UIFont.boldSystemFont(ofSize: 30)
         label.text = "\(value)"
+        if value > 4 {
+            label.textColor = .white
+        }
         addSubview(label)
+    }
+    
+    private func animateAppearance() {
+        transform = CGAffineTransform.identity.scaledBy(x: 0.5, y: 0.5)
+        UIView.animate(withDuration: animationDuration) {
+            self.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 1)
+        }
+    }
+    
+    private func animateMerge() {
+        UIView.animate(withDuration: animationDuration,
+                       animations: {
+                        self.transform = CGAffineTransform.identity.scaledBy(x: 1.1, y: 1.1)
+        }) { completed in
+            UIView.animate(withDuration: self.animationDuration) {
+                self.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 1)
+            }
+        }
+    }
+    
+    private func animateDisappearance() {
+        UIView.animate(withDuration: 0.2) {
+            self.alpha = 0.0
+        }
     }
 }
